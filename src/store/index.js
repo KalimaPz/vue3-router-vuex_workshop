0691d1478ef1cf6e002681faaec53f34a5ef661c
KalimaPz <kalima007c@gmail.com>
2021-01-16 01:08:23 +0700

    setting up api store data

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,37 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import {
+  db
+} from "../config/config";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    count : 0
+    loading : true,
+    biograph: [],
+    info : {},
+    lifestyle : [],
+    skill : {},
+    story : '',
+    work : [],
   },
   mutations: {
-    increment () {
-    this.state.count++
+    add_profile_detail(state, data) {
+      state.info = data.info
+      state.lifestyle = data.lifestyle
+      state.skill = data.siklls
+      state.story = data.story
+      state.work = data.work_exp
     }
   },
+
   actions: {
-    INCREASE(context) {
-      context.commit('increment')
+    async FETCH (context) {
+      const snapshot = await db.collection('information').doc('8NB5BqxpGYzk5lA1QSml').get()
+      const data = snapshot.data()
+      console.log(data)
+      context.commit('add_profile_detail', snapshot.data())
     }
   },
   modules: {}
