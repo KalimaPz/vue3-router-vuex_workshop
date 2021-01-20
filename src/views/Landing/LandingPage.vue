f4b82a790c7ca511e76ef2cb37bb2539cc9cc3df
KalimaPz <kalima007c@gmail.com>
2021-01-21 01:23:58 +0700

    landing page - load screen

---
+++ src/views/Landing/LandingPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="landing-page">
+    <div class="after">sss</div>
+    <div class="backgroundImage"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage",
+};
+</script>
+
+<style scoped>
+.after {
+  content: "";
+  position: absolute;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  -webkit-backdrop-filter: blur(50px); /* apply the blur */
+  backdrop-filter: blur(50px); /* apply the blur */
+  pointer-events: none; /* make the pseudo class click-through */
+}
+.backgroundImage {
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-image: url("../../assets/2061623.jpg");
+  overflow-x: hidden;
+}
+</style>
---
+++ src/views/Loading/LoadingScreen.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <v-progress-circular />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingScreen",
+};
+</script>
+
+<style scoped>
+</style>
